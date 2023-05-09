0c1fa555a7f015f2ee13305f01f23274cd338774
supinyu <supinyu@126.com>
2023-05-09 20:04:29 +0800

    add test code
    add test code

---
+++ datasets/auto_knowledage_extract_cat/data_extract.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/9 17:21
+# @Author  : supinyu
+# @File    : data_extract.py.py
+import json
+import random
+
+from datasets import load_dataset
+
+
+def data_util(file_input):
+    data_list = []
+    with open(file_input, "r") as f:
+        for line in f:
+            lines = json.loads(line.strip())
+            text = lines["text"]
+            spo_data = lines["spo_list"]
+            spo_list = []
+            for item in spo_data:
+                h_name = item["h"]["name"]
+                t_name = item["t"]["name"]
+                relation = item["relation"]
+                spo_list.append(h_name + "_" + relation + "_" + t_name)
+            if len(spo_list) > 0:
+                data_list.append((text, "\n".join(spo_list)))
+    random.shuffle(data_list)
+    eval_data = data_list[0:100]
+    eval_data_list = [{"text": item[0], "completion": item[1]} for item in eval_data]
+    train_data = data_list[100:]
+    train_data_list = [{"text": item[0], "completion": item[1]} for item in train_data]
+
+    with open("auto_knowledge_car_llm_train.json", "w") as fw:
+        fw.write(json.dumps(train_data_list, indent=4, ensure_ascii=False))
+    with open("auto_knowledge_car_llm_eval.json", "w") as fw:
+        fw.write(json.dumps(eval_data_list, indent=4, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    # data_util("auto_knowledge_car_train.json")
+    data_file_dict = {}
+    data_extension = "json"
+    data_file_dict["train"] = "auto_knowledge_car_llm_train.json"
+    data_file_dict["validation"] = "auto_knowledge_car_llm_eval.json"
+    local_dataset = load_dataset(data_extension, data_files=data_file_dict)
+    print(local_dataset)
---
+++ model_zoo/ChatGLM-6B/model_code/README.md
@@ -0,0 +1,8 @@
+# 目录介绍
+
+主要同步相关代码
+
+最新同步时间：2023-04-23
+
+https://huggingface.co/THUDM/chatglm-6b/tree/main
+
