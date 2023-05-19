8cdd724fe3842108c3f1bd1e94a8d596695f8155
supinyu <supinyu@126.com>
2023-05-19 17:49:24 +0800

    add set args
    add set args

--- model_zoo/ChatGLM-6B/lora/v3/acclerator_train_lora.py
+++ model_zoo/ChatGLM-6B/lora/v3/acclerator_train_lora.py
@@ -2,6 +2,7 @@
 # @Time    : 2023/5/19 16:39
 # @Author  : supinyu
 # @File    : acclerator_train_lora.py.py
+import argparse
 import logging
 import os
 import sys
@@ -12,7 +13,7 @@ import torch
 import numpy as np
 import loralib as lora
 from datasets import load_dataset
-from peft import LoraConfig
+from peft import LoraConfig, get_peft_model
 
 from arguments import ModelArguments, DataTrainingArguments, FineTuneArguments
 from torch.utils.data import DataLoader
@@ -24,11 +25,35 @@ from transformers import get_linear_schedule_with_warmup
 logger = logging.getLogger(__name__)
 log_name = __name__
 
+def set_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_path', default='data/spo_0.json', type=str, help='')
+    parser.add_argument('--model_dir', default="/data/work/lcong/public_model_path/ChatGLM-6B/", type=str, help='')
+    parser.add_argument('--num_train_epochs', default=5, type=int, help='')
+    parser.add_argument('--per_device_train_batch_size', default=2, type=int, help='')
+    parser.add_argument('--learning_rate', default=1e-5, type=float, help='')
+    parser.add_argument('--output_dir', default='output_dir_lora/', type=str, help='')
+    parser.add_argument('--max_source_length', type=int, default=768, help='')
+    parser.add_argument('--max_target_length', type=int, default=450, help='')
+    parser.add_argument('--seed', type=int, default=42, help='')
+    parser.add_argument('--lora_rank', type=int, default=8, help='')
+    parser.add_argument('--prompt_column', type=str, default="text", help='')
+    parser.add_argument('--response_column', type=str, default="response_column", help='')
+    parser.add_argument('--source_prefix', type=str,
+                        default="你现在是一个信息抽取模型，请你帮我抽取出关系内容为\"性能故障\", \"部件故障\", \"组成\"和 \"检测工具\"的相关三元组，三元组内部用\"_\"连接，三元组之间用\\n分割。文本：",
+                        help='')
+    return parser.parse_args()
 
 def main():
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, FineTuneArguments))
+    args = set_args()
     writer = SummaryWriter()
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    accumulate_step = 8
+    mixed_precision = 'bf16'
+    deepspeed_plugin = DeepSpeedPlugin(gradient_accumulation_steps=accumulate_step)
+    accelerator = Accelerator(mixed_precision=mixed_precision, deepspeed_plugin=deepspeed_plugin,
+                              log_with="tensorboard",
+                              project_dir='runs/')
+
     # Setup logging
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -38,24 +63,22 @@ def main():
     )
 
     # Set seed before initializing model.
-    set_seed(training_args.seed)
+    set_seed(args.seed)
 
     # Load dataset
     data_files = {}
-    data_files["train"] = data_args.train_file
-    extension = data_args.train_file.split(".")[-1]
+    data_files["train"] = args.train_file
+    extension = args.train_file.split(".")[-1]
 
     raw_datasets = load_dataset(
         extension,
-        data_files=data_files,
-        cache_dir=model_args.cache_dir,
-        use_auth_token=True if model_args.use_auth_token else None,
+        data_files=data_files
     )
 
-    model = AutoModel.from_pretrained(model_args.model_dir, trust_remote_code=True)
-    tokenizer = AutoTokenizer.from_pretrained(model_args.model_dir, trust_remote_code=True)
+    model = AutoModel.from_pretrained(args.model_dir, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_dir, trust_remote_code=True)
 
-    config = LoraConfig(r=training_args.lora_rank,
+    config = LoraConfig(r=args.lora_rank,
                         lora_alpha=32,
                         target_modules=["query_key_value"],
                         lora_dropout=0.1,
@@ -64,29 +87,28 @@ def main():
                         inference_mode=False,
                         )
 
-    accumulate_step = 8
-    mixed_precision = 'bf16'
+    model = get_peft_model(model, config)
+    model = model.half()
+    model.print_trainable_parameters()
+
     warm_up_ratio = 0.1
-    lr = training_args.learning_rate
-    batch_size = training_args.per_device_train_batch_size
-    num_epoch = training_args.num_train_epochs
-    deepspeed_plugin = DeepSpeedPlugin(gradient_accumulation_steps=accumulate_step)
-    accelerator = Accelerator(mixed_precision=mixed_precision, deepspeed_plugin=deepspeed_plugin,
-                              log_with="tensorboard",
-                              project_dir='runs/')
+    lr = args.learning_rate
+    batch_size = args.per_device_train_batch_size
+    num_epoch = args.num_train_epochs
+
     device = accelerator.device
 
     accelerator.wait_for_everyone()
 
     model.use_cache = False
 
-    prompt_column = data_args.prompt_column
-    response_column = data_args.response_column
-    history_column = data_args.history_column
-    prefix = data_args.source_prefix if data_args.source_prefix is not None else ""
+    prompt_column = args.prompt_column
+    response_column = args.response_column
+    history_column = None
+    prefix = args.source_prefix if args.source_prefix is not None else ""
 
     def preprocess_function_train(examples):
-        max_seq_length = data_args.max_source_length + data_args.max_target_length
+        max_seq_length = args.max_source_length + args.max_target_length
         model_inputs = {
             "input_ids": [],
             "labels": [],
@@ -108,11 +130,11 @@ def main():
                 a_ids = tokenizer.encode(text=prompt, add_special_tokens=False)
                 b_ids = tokenizer.encode(text=answer, add_special_tokens=False)
 
-                if len(a_ids) > data_args.max_source_length - 1:
-                    a_ids = a_ids[: data_args.max_source_length - 1]
+                if len(a_ids) > args.max_source_length - 1:
+                    a_ids = a_ids[: args.max_source_length - 1]
 
-                if len(b_ids) > data_args.max_target_length - 2:
-                    b_ids = b_ids[: data_args.max_target_length - 2]
+                if len(b_ids) > args.max_target_length - 2:
+                    b_ids = b_ids[: args.max_target_length - 2]
 
                 input_ids = tokenizer.build_inputs_with_special_tokens(a_ids, b_ids)
 
@@ -123,7 +145,7 @@ def main():
                 pad_len = max_seq_length - len(input_ids)
                 input_ids = input_ids + [tokenizer.pad_token_id] * pad_len
                 labels = labels + [tokenizer.pad_token_id] * pad_len
-                if data_args.ignore_pad_token_for_loss:
+                if True:
                     labels = [(l if l != tokenizer.pad_token_id else -100) for l in labels]
 
                 model_inputs["input_ids"].append(input_ids)
@@ -136,17 +158,14 @@ def main():
     if "train" not in raw_datasets:
         raise ValueError("--do_train requires a train dataset")
     train_dataset = raw_datasets["train"]
-    if data_args.max_train_samples is not None:
-        max_train_samples = min(len(train_dataset), data_args.max_train_samples)
-    train_dataset = train_dataset.select(range(max_train_samples))
     column_names = train_dataset.column_names
     with accelerator.main_process_first(desc="train dataset map pre-processing"):
         train_dataset = train_dataset.map(
             preprocess_function_train,
             batched=True,
-            num_proc=data_args.preprocessing_num_workers,
+            num_proc=1,
             remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
+            load_from_cache_file=False,
             desc="Running tokenizer on train dataset",
         )
 
--- model_zoo/ChatGLM-6B/lora/v3/train_lora.sh
+++ model_zoo/ChatGLM-6B/lora/v3/train_lora.sh
@@ -1,8 +1,8 @@
 MODEL_PATH=/disk/nlp_info/LLM_model/ChatGLM-6B
 source_prefix_str="你现在是一个信息抽取模型，请你帮我抽取出关系内容为\"性能故障\", \"部件故障\", \"组成\"和 \"检测工具\"的相关三元组，三元组内部用\"_\"连接，三元组之间用\\n分割。文本："
 
-accelerate launch --config_file config/default_config.yaml train_deepspeed.py \
-   --train_file /data/supinyu/github/Easy-NLP-Toolkit/datasets/auto_knowledage_extract_cat/auto_knowledge_car_llm_train.json \
+accelerate launch --config_file acclerator_config.yaml train_deepspeed.py \
+  --train_file /data/supinyu/github/Easy-NLP-Toolkit/datasets/auto_knowledage_extract_cat/auto_knowledge_car_llm_train.json \
   --model_name_or_path $MODEL_PATH \
   --output_dir lora_output \
   --prompt_column text \
